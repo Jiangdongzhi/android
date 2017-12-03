a2268f7233c139db8e215d03f1cdcb2a10dfe2f2
Dorian <dorian@hz.cn>
2017-12-04 00:57:44 +0800

    added one button

--- Dorianns/app/src/main/java/dorian/com/dorianns/MainActivity.java
+++ Dorianns/app/src/main/java/dorian/com/dorianns/MainActivity.java
@@ -1,7 +1,10 @@
 package dorian.com.dorianns;
 
+import android.support.v4.widget.TextViewCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClickProduce(View view) {
+        TextView content = (TextView) findViewById(R.id.content_pane);
+
+        content.setText("1, 3, 4, 5, 8");
     }
 }
